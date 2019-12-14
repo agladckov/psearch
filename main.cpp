4e071d8a14812d4340505be0214c5d6961ee0411
agladckov <56593858+agladckov@users.noreply.github.com>
2019-12-14 04:19:45 +0300

    Formatting

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <bits/stdc++.h>
@@ -13,14 +14,13 @@
 #include <thread>
 using namespace std;
 
-namespace filelist
-{
-    queue <string> filequeue;
-    //char initdirpath[MAXDIRPATH];
-    string initdirpath;
-    bool onlycur;
-    bool listmade;
-    char *pat;
+namespace filelist {
+queue<string> filequeue;
+//char initdirpath[MAXDIRPATH];
+string initdirpath;
+bool onlycur;
+bool listmade;
+char* pat;
 }
 
 //Preliminary declarations
@@ -41,37 +41,33 @@ void KMPSearch(char* pat, char* txt, char* file, unsigned int txtlen)
 
     unsigned int i = 0; // index for txt[]
     unsigned int j = 0; // index for pat[]
-		unsigned int strnum = 1;
-    while (i < txtlen)
-    {
-				if (txt[i] == '\n')
-					strnum++;
-        if (pat[j] == txt[i])
-        {
+    unsigned int strnum = 1;
+    while (i < txtlen) {
+        if (txt[i] == '\n')
+            strnum++;
+        if (pat[j] == txt[i]) {
             j++;
             i++;
         }
-        if (j == patlen)
-        {
+        if (j == patlen) {
             unsigned int cur = i - j;
-						while ((cur > 0) && (txt[cur] != '\n'))
-							cur--;
-						cur++;
-						unsigned int strbegin, strend;
-						strbegin = cur;
-						cur = i - j;
-						//vector <char> output;
-						while ((cur < txtlen - 1) && (txt[cur] != '\n'))
-							cur++;
-						strend = cur - 1;
+            while ((cur > 0) && (txt[cur] != '\n'))
+                cur--;
+            cur++;
+            unsigned int strbegin, strend;
+            strbegin = cur;
+            cur = i - j;
+            //vector <char> output;
+            while ((cur < txtlen - 1) && (txt[cur] != '\n'))
+                cur++;
+            strend = cur - 1;
             if (strnum != lastprinted)
-              printf("%s : %d : %.*s\n", file, strnum, strend - strbegin + 1, txt + strbegin);
+                printf("%s : %d : %.*s\n", file, strnum, strend - strbegin + 1, txt + strbegin);
             lastprinted = strnum;
-        		j = lps[j - 1];
+            j = lps[j - 1];
         }
         // mismatch after j matches
-        else if (i < txtlen && pat[j] != txt[i])
-        {
+        else if (i < txtlen && pat[j] != txt[i]) {
             // Do not match lps[0..lps[j-1]] characters,
             // they will match anyway
             if (j != 0)
@@ -79,7 +75,6 @@ void KMPSearch(char* pat, char* txt, char* file, unsigned int txtlen)
             else
                 i = i + 1;
         }
-
     }
 }
 
@@ -121,183 +116,164 @@ void computeLPSArray(char* pat, unsigned int M, unsigned int* lps)
 
 void makefilelist()
 {
-  godir(filelist::initdirpath);
-  filelist::listmade = true;
+    godir(filelist::initdirpath);
+    filelist::listmade = true;
 }
-
 
 int searchinfile()
 {
-  while (filelist::filequeue.empty())
-  {
-    if (filelist::listmade)
-      return 0;
-  }
-  while (not filelist::filequeue.empty())
-  {
-    string strcurfile = filelist::filequeue.front();
-    filelist::filequeue.pop();
-    char *curfile = new char[strcurfile.length() + 1];
-    curfile = strcpy(curfile, strcurfile.c_str());
-    int fd = open(curfile, O_RDWR, 0600);
-    if (fd < 0)
-    {
-      perror(curfile);
-      delete[] curfile;
-      close(fd);
-      continue;
+    while (filelist::filequeue.empty()) {
+        if (filelist::listmade)
+            return 0;
     }
-    unsigned long long fsize = 0;
-    char *buf = new char[1];
-    lseek(fd, 0, SEEK_SET);
-    while (1 == read(fd, buf, 1))
-    {
-      fsize++;
-      lseek(fd, 1, SEEK_CUR);
+    while (not filelist::filequeue.empty()) {
+        string strcurfile = filelist::filequeue.front();
+        filelist::filequeue.pop();
+        char* curfile = new char[strcurfile.length() + 1];
+        curfile = strcpy(curfile, strcurfile.c_str());
+        int fd = open(curfile, O_RDWR, 0600);
+        if (fd < 0) {
+            perror(curfile);
+            delete[] curfile;
+            close(fd);
+            continue;
+        }
+        unsigned long long fsize = 0;
+        char* buf = new char[1];
+        lseek(fd, 0, SEEK_SET);
+        while (1 == read(fd, buf, 1)) {
+            fsize++;
+            lseek(fd, 1, SEEK_CUR);
+        }
+        void* memstart = mmap(NULL, fsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+        KMPSearch(filelist::pat, (char*)memstart, curfile, fsize);
+
+        munmap(memstart, fsize);
+        close(fd);
+        delete[] curfile;
+        delete[] buf;
     }
-    void *memstart = mmap(NULL, fsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-
-    KMPSearch(filelist::pat, (char *) memstart, curfile, fsize);
-
-    munmap(memstart, fsize);
-    close(fd);
-    delete[] curfile;
-    delete[] buf;
-  }
-  return 0;
+    return 0;
 }
 
 int godir(string dirpath)
 {
-	DIR *dir = opendir(&dirpath[0]);
-	if (dir == NULL)
-	{
-		perror(&dirpath[0]);
-		return -1;
-	}
-	struct dirent *rd = readdir(dir);
-	while (rd != NULL)
-	{
-		while (((rd->d_type != DT_REG) && (rd->d_type != DT_DIR)) || (rd->d_type == DT_FIFO))
-			rd = readdir(dir);
-		if (rd->d_type == DT_REG)
-      filelist::filequeue.push(string(dirpath) + "/" + string(rd->d_name));
-		else if ((not filelist::onlycur) && (rd->d_type == DT_DIR))
-		{
-      if (strcmp(rd->d_name, ".") == 0 || strcmp(rd->d_name, "..") == 0)
-			{
-				rd = readdir(dir);
-				continue;
-			}
-      string newdirpath = (string) dirpath + "/" + (string) rd->d_name;
-      godir(newdirpath);
-			}
-		rd = readdir(dir);
+    DIR* dir = opendir(&dirpath[0]);
+    if (dir == NULL) {
+        perror(&dirpath[0]);
+        return -1;
+    }
+    struct dirent* rd = readdir(dir);
+    while (rd != NULL) {
+        while (((rd->d_type != DT_REG) && (rd->d_type != DT_DIR)) || (rd->d_type == DT_FIFO))
+            rd = readdir(dir);
+        if (rd->d_type == DT_REG)
+            filelist::filequeue.push(string(dirpath) + "/" + string(rd->d_name));
+        else if ((not filelist::onlycur) && (rd->d_type == DT_DIR)) {
+            if (strcmp(rd->d_name, ".") == 0 || strcmp(rd->d_name, "..") == 0) {
+                rd = readdir(dir);
+                continue;
+            }
+            string newdirpath = (string)dirpath + "/" + (string)rd->d_name;
+            godir(newdirpath);
+        }
+        rd = readdir(dir);
+    }
+    free(rd);
+    closedir(dir);
+    return 0;
 }
-  free(rd);
-	closedir(dir);
-	return 0;
-}
-
 
 int main(int argc, char** argv)
 {
-	int patlen = -1;
-	int numstreams = 1;
-	filelist::onlycur = 0;
-  filelist::listmade = false;
-  filelist::initdirpath = ".";
-  //Чтение аргументов
-  {
-	if (argc == 1)
-	{
-		printf("Enter the pattern to search for\n");
-		return 0;
-	}
-	else if (argc > 1)
-	{
-		int i = 1;
-		while (i < argc)
-		{
-			char c = argv[i][0];
-			if (c == '-')
-			{
-				c = argv[i][1];
-				if (c == 'n')
-					filelist::onlycur = 1;
-				else if (c == 't')
-				{
-					string cnumstreams = "";
-					int j = 2;
-					while (argv[i][j] != '\0')
-					{
-						cnumstreams = cnumstreams + argv[i][j];
-						j++;
-					}
-					if (numstreams > 1)
-					{
-						printf("There cannot be two or more -t options\n");
-						return 0;
-					}
-          numstreams = stoi(cnumstreams);
-          if (numstreams < 1)
-          {
-            printf("Enter a valid number of streams\n");
-            return 0;
-          }
-				}
-			}
-			else if (c == '/')
-			{
-				int j = 0;
-				//предполагаем, что путь начинается не со слеша
-				//dirpath[0] = '\';
-				if (filelist::initdirpath[0] != '.')
-				{
-					printf("You cannot specify two or more folders\n");
-					return 0;
-				}
-				while (argv[i][j] != '\0')
-				{
-					filelist::initdirpath = filelist::initdirpath +argv[i][j];
-					j++;
-				}
-			}
-			else
-			{
-				patlen = 0;
-				while (argv[i][patlen] != '\0')
-					patlen++;
-				filelist::pat = new char[patlen + 1];
-				int j = 0;
-				for (; j < patlen; j++)
-					filelist::pat[j] = argv[i][j];
-				filelist::pat[j] = '\0';
-			}
-			i++;
-		}
-	}
-}
-  //Завершен блок чтения аргументов
-
-  //Запуск нужного количества потоков
-
-  std::thread thrgo[numstreams];
-  std::thread thrmake(makefilelist);
-
-  if (numstreams > 1)
-  {
-    for (int i = 0; i < numstreams - 1; i++)
+    int patlen = -1;
+    int numstreams = 1;
+    filelist::onlycur = 0;
+    filelist::listmade = false;
+    filelist::initdirpath = ".";
+    //Чтение аргументов
     {
-      thrgo[i] = std::thread(searchinfile);
+        if (argc == 1) {
+            printf("Enter the pattern to search for\n");
+            return 0;
+        }
+        else if (argc > 1) {
+            int i = 1;
+            while (i < argc) {
+                char c = argv[i][0];
+                if (c == '-') {
+                    c = argv[i][1];
+                    if (c == 'n')
+                        filelist::onlycur = 1;
+                    else if (c == 't') {
+                        string cnumstreams = "";
+                        int j = 2;
+                        while (argv[i][j] != '\0') {
+                            cnumstreams = cnumstreams + argv[i][j];
+                            j++;
+                        }
+                        if (numstreams > 1) {
+                            printf("There cannot be two or more -t options\n");
+                            return 0;
+                        }
+                        numstreams = stoi(cnumstreams);
+                        if (numstreams < 1) {
+                            printf("Enter a valid number of streams\n");
+                            return 0;
+                        }
+                    }
+                }
+                else if (c == '/') {
+                    int j = 0;
+                    //предполагаем, что путь начинается не со слеша
+                    //dirpath[0] = '\';
+                    if (filelist::initdirpath[0] != '.') {
+                        printf("You cannot specify two or more folders\n");
+                        return 0;
+                    }
+                    while (argv[i][j] != '\0') {
+                        filelist::initdirpath = filelist::initdirpath + argv[i][j];
+                        j++;
+                    }
+                }
+                else {
+                    patlen = 0;
+                    while (argv[i][patlen] != '\0')
+                        patlen++;
+                    filelist::pat = new char[patlen + 1];
+                    int j = 0;
+                    for (; j < patlen; j++)
+                        filelist::pat[j] = argv[i][j];
+                    filelist::pat[j] = '\0';
+                }
+                i++;
+            }
+        }
     }
-  }
-  thrmake.join();
-  searchinfile();
+    //Завершен блок чтения аргументов
 
-  for (int i = 0; i < numstreams - 1; i++)
-  {
-      thrgo[i].join();
-  }
-  delete[] filelist::pat;
+    //Запуск нужного количества потоков
+
+    std::thread thrgo[numstreams];
+    std::thread thrmake(makefilelist);
+
+    if (numstreams > 1) {
+        for (int i = 0; i < numstreams - 1; i++) {
+            thrgo[i] = std::thread(searchinfile);
+        }
+    }
+    thrmake.join();
+    searchinfile();
+
+    for (int i = 0; i < numstreams - 1; i++) {
+        thrgo[i].join();
+    }
+    delete[] filelist::pat;
 }
+
+
+Format!Style:
+C++ online code formatter © 2014 by KrzaQ
+
+Powered by vibe.d, the D language and clang-format
